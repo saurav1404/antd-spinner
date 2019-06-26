6eb2518bb4ee22271b93d4db473bd1c02044fcc5
Saurav Suman <sanspoly@gmail.com>
2019-06-26 20:27:01 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import socketIOClient from "socket.io-client";
-import { Spin, Icon } from "antd";
+import { Progress } from "antd";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "./styles.css";
-
-const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 
 class App extends Component {
   constructor() {
@@ -27,9 +25,9 @@ class App extends Component {
     return (
       <div style={{ textAlign: "center" }}>
         {response ? (
-          <p>The temperature in Florence is: {response} °F</p>
+          <Progress type="circle" percent={response} />
         ) : (
-          <Spin indicator={antIcon} />
+          <Progress type="circle" percent={0} />
         )}
       </div>
     );
